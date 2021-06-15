ee28bb2dfe4bcd746f18367b8bdcd2d46fcb9205
Engin Yenice <enginyenice2626@gmail.com>
2021-06-15 10:24:12 +0300

    TokenService / CreateRefreshToken

---
+++ AuthServer.Service/Services/TokenService.cs
@@ -0,0 +1,49 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.Dto;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly UserManager<UserApp> _userManager;
+        private readonly CustomTokenOption _tokenOption;
+
+        
+        public TokenService(IOptions<CustomTokenOption> tokenOption, UserManager<UserApp> userManager)
+        {
+            _tokenOption = tokenOption.Value;
+            _userManager = userManager;
+        }
+        private string CreateRefreshToken()
+        {
+            // System.Security.Cryptography; kütüphanesi ile random number üretme ve onu byte'a çevirme
+            var numberByte = new Byte[32];
+            using var rnd = RandomNumberGenerator.Create();
+            rnd.GetBytes(numberByte);
+
+
+            return Convert.ToBase64String(numberByte);
+        }
+
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
