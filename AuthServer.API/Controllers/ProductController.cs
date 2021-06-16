61fefa38709bf57e8a3717a89ad9193dd1c02f9c
Engin Yenice <enginyenice2626@gmail.com>
2021-06-16 18:37:46 +0300

    ProductController

--- AuthServer.API/Controllers/ProductController.cs
+++ AuthServer.API/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using AuthServer.Core.Dto;
 using AuthServer.Core.Models;
 using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -10,6 +11,7 @@ using System.Threading.Tasks;
 
 namespace AuthServer.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ProductController : CustomBaseController
