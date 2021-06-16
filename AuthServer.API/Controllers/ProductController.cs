1e4c780ddeb066b54294d03613f2fc3a1a93ed80
Engin Yenice <enginyenice2626@gmail.com>
2021-06-16 18:39:07 +0300

    ProductController

--- AuthServer.API/Controllers/ProductController.cs
+++ AuthServer.API/Controllers/ProductController.cs
@@ -37,7 +37,7 @@ namespace AuthServer.API.Controllers
         {
             return ActionResultInstance(await _genericService.Update(productDto,productDto.Id));
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteProduct(int id)
         {
             return ActionResultInstance(await _genericService.Remove(id));
